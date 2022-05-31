a1b47111848a817e89f0f49862274741b0450b29
Silviu Oprea <silviu.oprea17@yahoo.com>
2022-05-31 14:00:57 +0300

    Initial commit

---
+++ src/main/java/ro/raccoon/recsys/RecsysApplication.java
@@ -0,0 +1,13 @@
+package ro.raccoon.recsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecsysApplication.class, args);
+	}
+
+}
